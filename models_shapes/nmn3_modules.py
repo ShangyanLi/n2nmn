930c05a8fedc26523929dbaea9edbeb45a0abab4
ShangyanLi <smellishangyan@gmail.com>
2017-12-08 02:32:32 -0500

    added prints for shapes

--- models_shapes/nmn3_modules.py
+++ models_shapes/nmn3_modules.py
@@ -64,11 +64,17 @@ class Modules:
             text_param_mapped = fc('fc_text', text_param, output_dim=map_dim)
             text_param_mapped = tf.reshape(text_param_mapped, to_T([N, 1, 1, map_dim]))
             text_param_mapped = tf.Print(text_param_mapped, [text_param_mapped], message="This is text_param_mapped: ", summarize=100)
+            text_param_mapped_sh = text_param_mapped.get_shape()
+            text_param_mapped_sh = tf.Print(text_param_mapped_sh, [text_param_mapped_sh], message="This is text_param_mapped_sh: ",
+                                         summarize=100)
             print('Shape of text_param_mapped: {}'.format(text_param_mapped.get_shape()))
             print('Shape of image_feat_mapped: {}'.format(image_feat_mapped.get_shape()))
             eltwise_mult = tf.nn.l2_normalize(image_feat_mapped * text_param_mapped, 3)
             eltwise_mult = tf.Print(eltwise_mult, [eltwise_mult], message="This is eltwise_mult: ",
                                          summarize=100)
+            eltwise_mult_sh = eltwise_mult.get_shape()
+            eltwise_mult_sh = tf.Print(eltwise_mult_sh, [eltwise_mult_sh], message="This is eltwise_mult_sh: ",
+                                    summarize=100)
             print('Shape of eltwise_mult: {}'.format(eltwise_mult.get_shape()))
             att_grid = _1x1_conv('conv_eltwise', eltwise_mult, output_dim=1)
             att_grid = tf.Print(att_grid, [att_grid], message="This is att_grid: ", summarize=100)
